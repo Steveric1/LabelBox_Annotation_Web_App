24b2ae2d81f4a38474cf666e6bd16c3dbf705a9f
Eric <stephen.erich243@gmail.com>
2024-12-19 13:06:40 +0000

    deployment build added

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Step 1: Install requirements.txt
+pip install -r requirements.txt
+
+# Step 2: Collect static files and run migrations
+python manage.py collectstatic --no-input
+
+# Step 3: migrate database
+python manage.py migrate
+
+# Step 4: Create a superuser
+echo "from django.contrib.auth.models import User; \
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists(): \
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
--- labelbox/settings.py
+++ labelbox/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t74##px2u%@gq-+m%(nqd-qa#54_vib5ek=mlob4-!6pusgq*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -78,12 +79,17 @@ WSGI_APPLICATION = 'labelbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if not DEBUG:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
