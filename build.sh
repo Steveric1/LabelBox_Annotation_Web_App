9a5a96421eb0e6c5c2e3603d49b2fa94a24f8be6
Eric <stephen.erich243@gmail.com>
2024-12-19 13:20:51 +0000

    updated

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python manage.py collectstatic --no-input
 # Step 3: migrate database
 python manage.py migrate
 
-# Step 4: Create a superuser
-echo "from django.contrib.auth.models import User; \
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists(): \
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+# # Step 4: Create a superuser
+# echo "from django.contrib.auth.models import User; \
+# if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists(): \
+#     User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
